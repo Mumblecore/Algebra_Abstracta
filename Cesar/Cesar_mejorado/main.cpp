c68fc07dd6ea0ba517a2b26fe469d0852d39f0b1
Mumblecore <62074079+Mumblecore@users.noreply.github.com>
2020-04-13 20:14:25 -0500

    Update main.cpp

--- Cesar/Cesar_mejorado/main.cpp
+++ Cesar/Cesar_mejorado/main.cpp
@@ -1,1 +1,15 @@
-fghfgh
+#include <iostream>
+#include <string>
+#include "Cesar.h"
+
+using namespace std;
+
+int main()
+{
+    Cesar a;
+
+    cout << a.cif("ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmonpqrstuv 1234567890,.") << "\n";
+    cout << a.des(a.cif("ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmonpqrstuv 1234567890,.")) << "\n";
+
+    return 0;
+}
