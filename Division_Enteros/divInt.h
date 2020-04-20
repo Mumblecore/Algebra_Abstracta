9bfff3d1fa88eefb6b0612e4d1933c87bd3811c7
Mumblecore <62074079+Mumblecore@users.noreply.github.com>
2020-04-19 20:16:47 -0500

    Create divInt.h

---
+++ Division_Enteros/divInt.h
@@ -0,0 +1,15 @@
+#ifndef DIVINT_H
+#define DIVINT_H
+
+
+class divInt
+{
+    public:
+        divInt(int, int);
+        int a;
+        int n;
+        int q;
+        int r;
+};
+
+#endif // DIVINT_H
