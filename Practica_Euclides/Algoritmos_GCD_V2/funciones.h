c7d62b4917e733b86b46bf8f66dc96b7afdef585
Alan Monroy Bernedo <alan@MacBook-Air.local>
2020-05-14 18:31:47 -0500

    Add Fstream

--- Practica_Euclides/Algoritmos_GCD_V2/funciones.h
+++ Practica_Euclides/Algoritmos_GCD_V2/funciones.h
@@ -6,10 +6,8 @@
 
 ZZ foo1(ZZ a, ZZ b)
 {
-    ZZ c(0);
     ZZ r = mod(a,b);
     while(r != 0){
-        cout << "vuelta: " << ++c << "\ta: " << a << "\tb: " << b << "\tr: " << r << endl;
         a = b;
         b = r;
         r = mod(a,b);
@@ -19,10 +17,8 @@ ZZ foo1(ZZ a, ZZ b)
 
 ZZ foo2(ZZ a, ZZ b)
 {
-    ZZ c(0);
     ZZ r = mod(a,b);
     while(r != 0){
-        cout << "vuelta: " << ++c <<"\ta: " << a << "\tb: " << b << "\tr: " << r << endl;
         if(r > b/2)
         {
             r = b - r;
@@ -34,14 +30,10 @@ ZZ foo2(ZZ a, ZZ b)
     return b;
 }
 
-ZZ cont_r(0);
-
 ZZ foo3(ZZ a, ZZ b)
 {
-    cout << "llamada " << ++cont_r << " :(" << a << ", " << b << ")" << endl;
     if(b==0)
     {
-        cont_r = 0;
         return a;
     }
     return foo3(b,mod(a,b));
@@ -49,14 +41,12 @@ ZZ foo3(ZZ a, ZZ b)
 
 ZZ foo4(ZZ a, ZZ b)
 {
-    cout << "llamada " << ++cont_r << " :(" << a << ", " << b << ")" << endl;
     if(abs(b) > abs(a))
     {
         return foo4(b,a);
     }
     if(b==0)
     {
-        cont_r = 0;
         return a;
     }
     if(!(mod(a,2)) && !(mod(b,2)))
@@ -77,38 +67,23 @@ ZZ foo4(ZZ a, ZZ b)
 ZZ foo5(ZZ a, ZZ b)
 {
     ZZ g(1);
-    ZZ c1(0);
-    cout << "While 1:\n";
-    cout << "\tvuelta: " << c1 << "\ta: " << a << "\tb: " << b << "\tg: " << g << endl;
     while(!(mod(a,2)) && !(mod(b,2)))
     {
         a/=2;
         b/=2;
         g*=2;
-        cout << "\tvuelta: " << ++c1 << "\ta: " << a << "\tb: " << b << "\tg: " << g << endl;
     }
 
-    ZZ c2(0);
-    ZZ c3(0);
-    ZZ c4(0);
-    cout << "While 2:\n";
-    cout << "\tvuelta: " << c2 << "\ta: " << a << "\tb: " << b << "\tg: " << g << endl;
     while(a!=0)
     {
-        cout << "\t\tWhile 2.1:\n";
-        cout << "\t\t\tvuelta: " << c3 << "\ta: " << a << "\tb: " << b << "\tg: " << g << endl;
         while(!(mod(a,2)))
         {
             a/=2;
-            cout << "\t\t\tvuelta: " << ++c3 << "\ta: " << a << "\tb: " << b << "\tg: " << g << endl;
         }
 
-        cout << "\t\tWhile 2.2:\n";
-        cout << "\t\t\tvuelta: " << c4 << "\ta: " << a << "\tb: " << b << "\tg: " << g << endl;
         while(!(mod(b,2)))
         {
             b/=2;
-            cout << "\t\t\tvuelta: " << ++c4 << "\ta: " << a << "\tb: " << b << "\tg: " << g << endl;
         }
             
         ZZ t = abs(a-b)/2;
@@ -120,16 +95,12 @@ ZZ foo5(ZZ a, ZZ b)
         {
             b = t;
         }
-        cout << "\tvuelta: " << ++c2 << "\ta: " << a << "\tb: " << b << "\tg: " << g << "\tt: " << t << endl;   
     }
-    cout << "Iteraciones totales: " << c1+c2+c3+c4 << endl;
     return g*b;
 }
 
 ZZ foo6(ZZ a, ZZ b)
 {
-    ZZ c(0);
-    cout << "vuelta: " << c << "\ta: " << a << "\tb: " << b << endl;
     while(a!=b)
     {
         if(a > b)
@@ -140,7 +111,6 @@ ZZ foo6(ZZ a, ZZ b)
         {
             b-=a;
         }
-        cout << "vuelta: " << c << "\ta: " << a << "\tb: " << b << endl; c++;
     }
     return a;
 }
--- Practica_Euclides/Algoritmos_GCD_V2/main.cpp
+++ Practica_Euclides/Algoritmos_GCD_V2/main.cpp
@@ -1,17 +1,18 @@
+#include <fstream>
 #include "funciones.h"
 
 int main()
 {
+   fstream f("nros.txt", ios_base::in);
+
    ZZ a, b;
-   cout << "Ingrese el primer numero: ";
-   cin >> a; 
-   cout << "Ingrese el segundo numero: ";
-   cin >> b;
+
+   f >> a >> b;
    
-   cout << endl << "Algoritmo 1: " << endl << foo1(a,b) << endl;
+   cout << foo1(a,b) << endl;
    cout << endl << "Algoritmo 2: " << endl << foo2(a,b) << endl;
-   cout << endl << "Algoritmo 3: " << endl << foo3(a,b) << endl;
-   cout << endl << "Algoritmo 4: " << endl << foo4(a,b) << endl;
+   //cout << endl << "Algoritmo 3: " << endl << foo3(a,b) << endl;
+   //cout << endl << "Algoritmo 4: " << endl << foo4(a,b) << endl;
    cout << endl << "Algoritmo 5: " << endl << foo5(a,b) << endl;
    cout << endl << "Algoritmo 6: " << endl << foo6(a,b) << endl;
 
